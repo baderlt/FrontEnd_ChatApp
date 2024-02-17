cf1480ec07c75cdd09ad77efadc2b087bd341477
baderlt <latrachbadr9@gmail.com>
2024-02-17 10:50:47 +0100

    Updates

--- assets/NewChat-0f16c6dc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-102e86e5.js";const r=()=>t.jsx("div",{children:"New Chate "});export{r as default};
---
+++ assets/NewChat-2d866e2e.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-53397097.js";const r=()=>t.jsx("div",{children:"New Chate "});export{r as default};
