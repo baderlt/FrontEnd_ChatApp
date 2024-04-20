244cb1b8ec4d90b52280f6db02f077faa6338093
baderlt <baderlatrache10@gmail.com>
2024-04-20 15:09:46 +0100

    optm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import { Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Nav_Bar from "../Compement/Nav_Bar";
 // import Chats from "../Compement/Chat";
-import React, { Suspense, createContext, useEffect, useRef, useState } from "react";
+import React, {
+  Suspense,
+  createContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 import { Server_Socket, baseUrl, colors } from "../touls";
 import Drawer_Info from "../Drawer_Profil";
-import useSound from 'use-sound';
+import useSound from "use-sound";
 import Not_Found from "./Not_Found";
 import Loading_App from "../Loding/Loading_App";
 import Chats from "../Compement/Chat";
 import NewChat from "../Compement/New_Chat/NewChat";
-import Notif_Sound from './notification.wav'
+import Notif_Sound from "./notification.wav";
 import useSendNotitication from "../Houks/useSendNotitication ";
 import getUsers from "./Promiss/getUsers";
 import GetNotifications from "./Promiss/GetNotifications";
@@ -23,35 +28,33 @@ export const OnlinUserContext = createContext(null);
 
 // const Lazy_chats = React.lazy(() => import("../Compement/Chat"));
 const Home = () => {
-
   const dispatch = useDispatch();
   const Info_User = useSelector((state) => state.Auth_check.user_Info);
   const Profile_Drawer = useSelector((state) => state.Drawer_Profile);
-  const [My_is_Socket,setMy_Id]=useState('');
+  const [My_is_Socket, setMy_Id] = useState("");
   const [Socket, SetSocket] = useState(null);
   const [onlineUser, SetOnlineUser] = useState();
   const [notifications, SetNotification] = useState([]);
   const openedChat = useSelector((state) => state.alert.openEdChat);
   const [play_notif] = useSound(Notif_Sound);
-  const [calleSignal,setCallerSignal] = useState()
-  const [callAccepted,setcallaccepted] = useState()
-  const connectionRef= useRef();
-  const [stream,setstream]=useState();
-  const myvedio=useRef();
-  const userVedio=useRef();
+  const [calleSignal, setCallerSignal] = useState();
+  const [callAccepted, setcallaccepted] = useState();
+  const connectionRef = useRef();
+  const [stream, setstream] = useState();
+  const myvedio = useRef();
+  const userVedio = useRef();
 
-  ///// inser the notification not read in database 
-  useSendNotitication(notifications,Info_User)
-  ///// handel connectionref 
-  const handleConnectionRef=(peer)=>{
-   connectionRef.current=peer;
-  }
+  ///// inser the notification not read in database
+  useSendNotitication(notifications, Info_User);
+  ///// handel connectionref
+  const handleConnectionRef = (peer) => {
+    connectionRef.current = peer;
+  };
 
   /////////////////////////////// updateNotification
   const updateNotification = (New_N) => {
     SetNotification(New_N);
   };
-
 
   ////////////////////// connexion  socket Io
   useEffect(() => {
@@ -65,28 +68,26 @@ const Home = () => {
     };
   }, [Info_User]);
 
-
-///////////////////////////////////////////////
-
+  ///////////////////////////////////////////////
 
   useEffect(() => {
     if (Socket === null) return;
-    //// get my is socket for use in call user 
-    Socket.on('me',(me)=>setMy_Id(me));
+    //// get my is socket for use in call user
+    Socket.on("me", (me) => setMy_Id(me));
     //// add me in list of onlign users
     Socket.emit("addNewUser", Info_User?._id);
-    //// gte the onlign users 
+    //// gte the onlign users
     Socket.on("onlineUser", (res) => {
       SetOnlineUser(res);
     });
 
-    ///// accepte the call 
-      Socket.on("callUser", (data) => {
-        setCallerSignal(data)
-      })
+    ///// accepte the call
+    Socket.on("callUser", (data) => {
+      setCallerSignal(data);
+    });
 
-     /////////////// get the notification  from socket on  action getNotification 
-      Socket.on("getNotification", (res) => {
+    /////////////// get the notification  from socket on  action getNotification
+    Socket.on("getNotification", (res) => {
       if (res.senderId === Info_User._id) return;
       const isChatOpen = openedChat?._id === res.senderId;
       if (isChatOpen) {
@@ -105,51 +106,60 @@ const Home = () => {
     };
   }, [Socket, openedChat]);
 
-  //// get userMedia 
-  const HandelUserMedia=()=>{
-        var getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia).bind(navigator);
-        //// start my vedio straming 
-        getUserMedia({
-            video: true,
-            audio: true 
-        },  (stream_) =>{    
-       setstream(stream_)
+  //// get userMedia
+  const HandelUserMedia = () => {
+    //// adapter for all browsers .
+    var getUserMedia = (
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia
+    ).bind(navigator);
+    //// start my vedio straming
+    getUserMedia(
+      {
+        video: true,
+        audio: true,
+      },
+      (stream_) => {
+        setstream(stream_);
 
-      answerCall(calleSignal.from,calleSignal.signal,stream_);
-            },(err)=>{console.log(err)});
-  }
+        answerCall(calleSignal.from, calleSignal.signal, stream_);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
 
-////////// accept the call ;
-  const answerCall =(caller,signal,mystream) =>  {
-    setcallaccepted(true)
-    try{
-          ///// my vedio streaming
-    myvedio.current.srcObject=mystream
-		const peer = new SimplePeer({
-			initiator: false,
-			trickle: false,
-			stream: stream
-		})
-		peer.on("signal", (data) => {
-			Socket.emit("answerCall", { signal: data, to: caller })
-		})
-		peer.on("stream", (stream_) => {
+  ////////// accept the call ;
+  const answerCall = (caller, signal, mystream) => {
+    setcallaccepted(true);
+    try {
+      ///// my vedio streaming
+      myvedio.current.srcObject = mystream;
+      const peer = new SimplePeer({
+        initiator: false,
+        trickle: false,
+        stream: stream,
+      });
+      peer.on("signal", (data) => {
+        Socket.emit("answerCall", { signal: data, to: caller });
+      });
+      peer.on("stream", (stream_) => {
+        ///// user vedio
+        userVedio.current.srcObject = stream_;
+      });
+      peer.signal(signal);
+      handleConnectionRef(peer);
+    } catch (e) {
+      console.log("e");
+    }
+  };
 
-    ///// user vedio 
-    userVedio.current.srcObject=stream_
-
-		})
-		peer.signal(signal)
-		handleConnectionRef(peer);
-  }catch(e){
-    console.log('e');
-  }
-	}
-
-///////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////
   useEffect(() => {
-    ///// use prommise get users for get all users 
-      getUsers(Info_User.token)
+    ///// use prommise get users for get all users
+    getUsers(Info_User.token)
       .then((res) => dispatch({ type: "users", payload: res.data }))
       .catch(() => {
         dispatch({
@@ -160,9 +170,8 @@ const Home = () => {
           },
         });
       });
-
-   ///// use promiss getNotification for get the notf of user   
-   GetNotifications(Info_User.token,Info_User._id)
+    ///// use promiss getNotification for get the notf of user
+    GetNotifications(Info_User.token, Info_User._id)
       .then((res) => SetNotification(res.data))
       .catch(() => {
         dispatch({
@@ -175,89 +184,115 @@ const Home = () => {
       });
     //  console.log( window.innerWidth)
   }, []);
- 
 
   ///// decline call;
-  const DeclineCall=()=>{
-    setCallerSignal(false)
-		connectionRef.current.destroy()
+  const DeclineCall = () => {
+    setCallerSignal(false);
+    connectionRef.current.destroy();
   };
-
 
   return (
     <>
-{calleSignal && !callAccepted? 
-    <div className="caling absolute top-2 left-[42%]  bg-slate-700 z-50 rounded-lg w-[400px]" style={{zIndex:1000}} >
-    <div className="pl-2 pr-2 ">     
-                 <div
-                  className={`p-2 flex flex-nowrap border-b-2 mt-1 border-gray-600/50 w-full`}
+      {calleSignal && !callAccepted ? (
+        <div
+          className="caling absolute top-2 left-[42%]  bg-slate-700 z-50 rounded-lg w-[400px]"
+          style={{ zIndex: 1000 }}
+        >
+          <div className="pl-2 pr-2 ">
+            <div
+              className={`p-2 flex flex-nowrap border-b-2 mt-1 border-gray-600/50 w-full`}
+            >
+              {Number.isInteger(Number(calleSignal.pic)) ? (
+                <AvatarReactjs
+                  name={calleSignal.name ? calleSignal.name : "user"}
+                  fontSize={"large"}
+                  backgroundColor={
+                    Number.isInteger(Number(calleSignal.pic))
+                      ? colors[calleSignal.pic]
+                      : "red"
+                  }
+                  fontColor={calleSignal.pic == 3 ? "black" : "azure"}
+                  width={"50px"}
+                  height={"50px"}
+                />
+              ) : (
+                <AvatarReactjs
+                  name={calleSignal.name ? calleSignal.name : "user"}
+                  fontSize={"large"}
+                  src={calleSignal.pic}
+                  width={"50px"}
+                  height={"50px"}
+                />
+              )}
+
+              <div className=" w-full grid grid-rows-2 grid-cols-3">
+                <span
+                  className="flex flex-row col-span-2 "
+                  style={{ width: "100%" }}
+                >
+                  <h2 className=" text-white pl-0 col-span-4 basis-1/2 ">
+                    &ensp; &ensp;{calleSignal.name}
+                  </h2>
+                </span>
+                <span className="row-span-2 flex justify-between items-center">
+                  <span
+                    className="flex justify-center items-center bg-green-500 p-2 rounded-lg "
+                    onClick={HandelUserMedia}
                   >
-                  {Number.isInteger(Number(calleSignal.pic)) ? (
-                    <AvatarReactjs
-                    name={calleSignal.name ?calleSignal.name : "user"}
-                    fontSize={"large"}
-                      backgroundColor={
-                        Number.isInteger(Number(calleSignal.pic))
-                          ? colors[calleSignal.pic]
-                          : "red"
-                      }
-                      fontColor={calleSignal.pic == 3 ? "black" : "azure"}
-                      width={"50px"}
-                      height={"50px"}
-                      />
-                      ) : (
-                        <AvatarReactjs
-                        name={calleSignal.name ?calleSignal.name : "user"}
-                        fontSize={"large"}
-                        src={calleSignal.pic}
-                        width={"50px"}
-                        height={"50px"}
-                        />
-                        )}
-
-                
-                  <div className=" w-full grid grid-rows-2 grid-cols-3">
-                    <span className="flex flex-row col-span-2 " style={{ width: "100%" }}>
-                      <h2 className=" text-white pl-0 col-span-4 basis-1/2 ">
-     
-                      &ensp;
-                      &ensp;{calleSignal.name}
-                      </h2>
-                    </span>
-                    <span className="row-span-2 flex justify-between items-center">
-                  <span className="flex justify-center items-center bg-green-500 p-2 rounded-lg " onClick={HandelUserMedia}><img src="call_voice_answer.gif" alt="call accept " width={30} height={30} /></span>
-                  <span className="flex justify-center items-center  bg-red-600 p-2 rounded-lg" onClick={DeclineCall}><img src="call_voice_end.png" alt="call accept " width={30} height={30} /></span>
-
-                  
-                  
+                    <img
+                      src="call_voice_answer.gif"
+                      alt="call accept "
+                      width={30}
+                      height={30}
+                    />
                   </span>
-                    <span className=" col-span-2">
-                      <p
-                        className={` text-gray-300 pl-2   w-full  `}
-                        style={{ width: "100%" }}
-                      >
-        
-        &ensp; voice call .... 
-                    
-                      </p>                    
-                    </span>
-                  </div>
-                </div>
-               
+                  <span
+                    className="flex justify-center items-center  bg-red-600 p-2 rounded-lg"
+                    onClick={DeclineCall}
+                  >
+                    <img
+                      src="call_voice_end.png"
+                      alt="call accept "
+                      width={30}
+                      height={30}
+                    />
+                  </span>
+                </span>
+                <span className=" col-span-2">
+                  <p
+                    className={` text-gray-300 pl-2   w-full  `}
+                    style={{ width: "100%" }}
+                  >
+                    &ensp; voice call ....
+                  </p>
+                </span>
               </div>
-      
-    </div> 
-    :""}
-<div className={`absolute m-0 pt-0 h-auto flex   bg-gray-200/45 rounded-lg  ${stream ? "":"hidden"}`} style={{zIndex:2000,width:"100%" ,height:"100%"}}>
-{/* {callAccepted  ?<div> */}
-<video playsInline muted ref={myvedio} autoPlay style={{ width: "200px",height:"200px" }} className="absolute left-0 top-0 " ></video>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div
+        className={`absolute m-0 pt-0 h-auto flex   bg-gray-200/45 rounded-lg  ${
+          stream ? "" : "hidden"
+        }`}
+        style={{ zIndex: 2000, width: "100%", height: "100%" }}
+      >
+        {/* {callAccepted  ?<div> */}
+        <video
+          playsInline
+          muted
+          ref={myvedio}
+          autoPlay
+          style={{ width: "200px", height: "200px" }}
+          className="absolute left-0 top-0 "
+        ></video>
 
-					<video playsInline muted ref={userVedio} autoPlay   >
-
-          </video>
-          {/* </div>:
+        <video playsInline muted ref={userVedio} autoPlay></video>
+        {/* </div>:
 					null} */}
-</div>
+      </div>
 
       <Drawer_Info
         User={Profile_Drawer.User}
@@ -265,7 +300,14 @@ const Home = () => {
       />
       {/* <body style={{overflow:'hidden'}}></body> */}
       <OnlinUserContext.Provider
-        value={{ onlineUser, notifications, Socket, updateNotification,My_is_Socket ,handleConnectionRef}}
+        value={{
+          onlineUser,
+          notifications,
+          Socket,
+          updateNotification,
+          My_is_Socket,
+          handleConnectionRef,
+        }}
       >
         <Nav_Bar />
 
@@ -274,7 +316,7 @@ const Home = () => {
             path={"/"}
             element={
               <Suspense fallback={<Loading_App />}>
-            <Chats/>
+                <Chats />
               </Suspense>
               // <Chats/>
             }
