1f2292afae792feae17b0461f43c4bd9e59ba151
baderlt <baderlatrache10@gmail.com>
2024-04-20 23:21:40 +0100

    optm

--- src/touls.jsx
+++ src/touls.jsx
@@ -1,5 +1,5 @@
-export const baseUrl="http://localhost:5000/api";
-// export const baseUrl="https://chat-app-x2of.onrender.com/api";
-// export const  Server_Socket="https://socket-y0xj.onrender.com/";
-export const  Server_Socket="http://localhost:3000/";
+// export const baseUrl="http://localhost:5000/api";
+export const baseUrl="https://chat-app-x2of.onrender.com/api";
+export const  Server_Socket="https://socket-y0xj.onrender.com/";
+// export const  Server_Socket="http://localhost:3000/";
 export const colors = [null, "green", "blue", "white", "gray", "red"];
