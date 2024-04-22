68e67949f147e7bf6e15dab1857d92d4d4e520ad
Bader Latarche <122647283+baderlt@users.noreply.github.com>
2024-04-22 21:23:16 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,8 +26,8 @@ export const OnlinUserContext = createContext(null);
 import Emitter from "../Events/stream_event";
 import Calling_User from "../Compement/caliing_user";
 //  const Lazy_chats=React.lazy(()=>{ import("../Compement/Chat") })
-
 // const Lazy_chats = React.lazy(() => import("../Compement/Chat"));
+
 const Home = () => {
   const dispatch = useDispatch();
   const Info_User = useSelector((state) => state.Auth_check.user_Info);
